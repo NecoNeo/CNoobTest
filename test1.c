a9d5dffff28249d17e28420eaf986df61f70c9c6
NecoNeo <neo2314@qq.com>
2018-06-03 11:29:02 +0800

    learn bytes

--- test1.c
+++ test1.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #define TRUE 1
 
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len)
+{
+    size_t i;
+    for (i = 0; i < len; i++) {
+        printf("%.2x", start[i]);
+    }
+    printf("\n");
+}
+
+void show_int(int x)
+{
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
 void main()
 {
     while (TRUE) {
         int c = getchar();
-        putchar(c);
+        show_int(c);
     }
 }
